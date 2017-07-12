0ee4dee60f935614ecf8ea31e320636a335b013b
Radu Suciu <radusuciu@gmail.com>
2017-07-12 12:09:01 -0700

    A few api methods

---
+++ dancealytics/api/__init__.py
@@ -0,0 +1,27 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import config.config as config
+import spotipy
+
+
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=config.config.SPOTIFY_CLIENT_ID,
+    client_secret=config.config.SPOTIFY_CLIENT_SECRET
+)
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def search(term):
+    return sp.search(term, type='track', limit=10)
+
+def autocomplete(term):
+    tracks = search(term)['tracks']['items']
+    return [(
+        track['id'],
+        ', '.join(a['name'] for a in track['artists']),
+        track['name'],
+        track['album']['images'][0]['url']
+    ) for track in tracks]
+
+def analyze(track_id):
+    return sp.audio_features(track_id)
--- dancealytics/views/api.py
+++ dancealytics/views/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import dancealytics.api as api
 
 
@@ -10,3 +10,20 @@ api_blueprint = Blueprint('api_blueprint', __name__,
 @api_blueprint.route('/')
 def index():
     return jsonify('hello')
+
+@api_blueprint.route('/search')
+def search():
+    term = request.args.get('term')
+
+    if term:
+        return jsonify(api.search(term))
+
+@api_blueprint.route('/autocomplete')
+def autocomplete():
+    term = request.args.get('term')
+    suggestions = api.autocomplete(term)
+    return jsonify(suggestions)
+
+@api_blueprint.route('/analyze/<track_id>')
+def analyze(track_id):
+    return jsonify(api.analyze(track_id))
