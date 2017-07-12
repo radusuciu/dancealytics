b0158104caadd349ab58722f016265dccbd30f0f
Radu Suciu <radusuciu@gmail.com>
2017-07-12 16:24:57 -0700

    Display analytics for each track

--- dancealytics/static/main.js
+++ dancealytics/static/main.js
@@ -4,9 +4,37 @@ var app = new Vue({
     el: '#app',
     data: {
         track: null,
+        analysis: null,
+        analysisCategories: [
+            ['danceability', 'orange'],
+            ['energy', 'yellow'],
+            ['loudness', 'red'],
+            ['liveness', 'green'],
+            ['acousticness', 'teal'],
+            ['speechiness', 'blue'],
+            ['instrumentalness', 'pink'],
+            ['valence', 'purple'],
+        ]
     },
     methods: {
+        analyze: function(track) {
+            this.track = track;
 
+            this.$http.get('/api/analyze/' + track.id).then(function(response) {
+                this.analysis = response.data[0];
+            }, function(error_response) {
+
+            });
+        }
+    },
+    filters: {
+        millisecondsToTime: function(milliseconds) {
+            var seconds = Math.round(milliseconds / 1000);
+            var minutes = Math.floor(seconds / 60);
+            var remainder = seconds % 60;
+            return minutes + ':' + ((remainder < 10) ? '0' : '') + remainder;
+        }
+    },
     mounted: function() {
         var me = this;
 
@@ -28,5 +56,14 @@ var app = new Vue({
                     return { results: results }
                 }
             },
+            onSelect: function(result) {
+                me.analyze({
+                    id: result.id,
+                    artist: result.description,
+                    title: result.title,
+                    image: result.image
+                });
+            }
+        });
     }
 });
