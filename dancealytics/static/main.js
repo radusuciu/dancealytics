722a14a1a2ea3b21d2f86eb8480befe1b85da0be
Radu Suciu <radusuciu@gmail.com>
2017-07-12 16:24:39 -0700

    Use semanti ui autocomplete

--- dancealytics/static/main.js
+++ dancealytics/static/main.js
@@ -1,11 +1,32 @@
 Vue.use(VueResource);
 
-var sidebar = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello world!',
+        track: null,
     },
     methods: {
 
+    mounted: function() {
+        var me = this;
+
+        $('.ui.search').search({
+            apiSettings: {
+                url: '/api/autocomplete?term={query}',
+                onResponse: function(response) {
+                    results = [];
+
+                    for (var i = 0, n = response.data.length; i < n; i++) {
+                        results.push({
+                            id: response.data[i][0],
+                            title: response.data[i][2],
+                            description: response.data[i][1],
+                            image: response.data[i][3]
+                        });
+                    }
+
+                    return { results: results }
+                }
+            },
     }
 });
