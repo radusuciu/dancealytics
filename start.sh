bad81cd05f37edae57d7e6ae744294e897fc692d
Radu Suciu <radusuciu@gmail.com>
2017-07-12 17:24:33 -0700

    Dockerize

--- start.sh
+++ start.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-export DEBUG='true'
-export FLASK_DEBUG=1
-export LANG='C.UTF-8'
-export LC_ALL='C.UTF-8'
-export FLASK_APP='dancealytics/__init__.py'
-
-# this is pretty specific to my system running Linux Subsystem for Windows with conda
-if [ ! -d venv ]; then
-    python3.5 -m venv --without-pip venv
-    source venv/bin/activate
-    curl https://bootstrap.pypa.io/get-pip.py | python
-    deactivate
-fi
-
+PROJECT_HOME="/home/dance/dancealytics"
+cd "${PROJECT_HOME}" || exit
+python3.5 -m venv venv
 source venv/bin/activate
-
 pip install -r requirements.txt
 
-# run the dev server
-flask run -h 0.0.0.0
+# run the server
+if [[ -n $DEBUG && $DEBUG == true ]]; then
+    flask run -h 0.0.0.0
+else
+    gunicorn --config=config/gunicorn.py dancealytics:app
+fi
